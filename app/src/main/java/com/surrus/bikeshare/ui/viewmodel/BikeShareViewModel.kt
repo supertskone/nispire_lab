34b6ce7140fb53b41ecc9e288bc9ca3559dd3a59
nemanja stamenovic <nemanja.stamenovic@gmail.com>
2021-11-27 14:08:24 +0100

    Initialize widget_ios_android

---
+++ app/src/main/java/com/surrus/bikeshare/ui/viewmodel/BikeShareViewModel.kt
@@ -0,0 +1,39 @@
+package com.surrus.bikeshare.ui.viewmodel
+
+import androidx.lifecycle.*
+import co.touchlab.kermit.Kermit
+import com.surrus.common.repository.CityBikesRepository
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+import java.util.*
+
+data class Country(val code: String, val displayName: String)
+
+class BikeShareViewModel(
+    private val cityBikesRepository: CityBikesRepository,
+    private val logger: Kermit
+) : ViewModel() {
+
+    val network = MutableStateFlow<String>("")
+    val stations = network.flatMapLatest { cityBikesRepository.pollNetworkUpdates(it) }
+
+    init {
+        viewModelScope.launch {
+            cityBikesRepository.fetchAndStoreNetworkList()
+        }
+    }
+
+    val groupedNetworks = cityBikesRepository.groupedNetworkList.map {
+        it.mapKeys {
+            val countryCode = it.key.lowercase(Locale.getDefault())
+            val locale = Locale("", countryCode)
+            val countryName = locale.displayCountry
+            Country(countryCode, countryName)
+        }
+    }
+
+    fun setCity(city: String) {
+        network.value = city
+    }
+}
